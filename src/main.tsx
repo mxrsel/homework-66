bbccb9aadd5d8cdcd1296e2ac037592ddfb11ebb
mxrsel <marsel14082006@gmail.com>
2024-11-01 00:21:01 +0600

    git init

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+
+createRoot(document.getElementById('root')!).render(
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+);
